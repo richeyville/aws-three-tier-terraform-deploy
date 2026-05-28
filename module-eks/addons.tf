46b15854df984a5f8911e4474246307bf9702a94
richeyville <richeyville@outlook.com>
2026-05-28 16:05:08 +0100

    Update addons.tf

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,63 +1,85 @@
 provider "helm" {
-    kubernetes {
-        host                   = aws_eks_cluster.eks.endpoint
-        cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-        token                  = data.aws_eks_cluster_auth.eks.token
+  kubernetes {
+    host                   = aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks.name]
+      command     = "aws"
     }
+  }
 }
 
 provider "kubernetes" {
   alias                  = "eks"
   host                   = aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks.name]
+    command     = "aws"
+  }
 }
 
 data "aws_eks_cluster_auth" "eks" {
-    name = aws_eks_cluster.eks.name
+  name = aws_eks_cluster.eks.name
 }
-resource "helm_release" "nginx_ingress" {
-    name       = "nginx-ingress"
-    repository = "https://kubernetes.github.io/ingress-nginx"
-    chart      = "ingress-nginx"
-    version    = "4.12.0"
-    namespace  = "ingress-nginx"
-    create_namespace = true
 
-    values = [file("${path.module}/nginx-ingress-values.yaml")]
-    depends_on = [ aws_eks_node_group.eks_node_group ]
+resource "helm_release" "nginx_ingress" {
+  name             = "nginx-ingress"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = "4.12.0"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+  timeout          = 600
+  wait             = true
+  atomic           = true
+  cleanup_on_fail  = true
+  values           = [file("${path.module}/nginx-ingress-values.yaml")]
+  depends_on       = [aws_eks_node_group.eks_node_group]
 }
 
 data "aws_lb" "nginx_ingress" {
   tags = {
-    "kubernetes.io/service-name" = "ingress-nginx/nginx-ingress-ingress-nginx-controller"
+    "kubernetes.io/cluster/production-eks-cluster" = "owned"
+  }
+  depends_on = [helm_release.nginx_ingress]
+}
+
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  version          = "1.14.5"
+  namespace        = "cert-manager"
+  create_namespace = true
+  timeout          = 900
+  wait             = true
+  atomic           = true
+  cleanup_on_fail  = true
+  force_update     = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
   }
 
   depends_on = [helm_release.nginx_ingress]
 }
 
-resource "helm_release" "cert_manager" {
-    name       = "cert-manager"
-    repository = "https://charts.jetstack.io"
-    chart      = "cert-manager"
-    version    = "1.14.5"
-    namespace  = "cert-manager"
-    create_namespace = true
-    set {
-        name  = "installCRDs"
-        value = "true"
-    }
-    depends_on = [ helm_release.nginx_ingress ]
-}
-#==================================================
-
 resource "helm_release" "argocd" {
-    name             = "argocd"
-    repository       = "https://argoproj.github.io/argo-helm"
-    chart            = "argo-cd"
-    version          = "5.51.6"
-    namespace        = "argocd"
-    create_namespace = true
-    values = [file("${path.module}/argocd-values.yaml")]
-    depends_on = [ helm_release.nginx_ingress, helm_release.cert_manager]
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "5.51.6"
+  namespace        = "argocd"
+  create_namespace = true
+  timeout          = 1200
+  wait             = false
+  atomic           = false
+  cleanup_on_fail  = true
+  force_update     = true
+  values           = [file("${path.module}/argocd-values.yaml")]
+  depends_on       = [helm_release.nginx_ingress, helm_release.cert_manager]
 }
