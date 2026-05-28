6d5b81710a45b27cd66e4aa8d9487fcfd38a65a2
richeyville <richeyville@outlook.com>
2026-05-28 16:10:36 +0100

    Update Terraform backend configuration

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "digitalwitchngbucketcloud1"
-    key    = "digitalwitchng/prodution/terraform.tfstate"
-    region = "us-west-1"
+    bucket = "richeyville-terraform-state-123"
+    key    = "prodution/terraform.tfstate"
+    region = "us-east-1"
   }
 }
