1382895f3766f4953e044fd51941e06c5ee16be2
richeyville <richeyville@outlook.com>
2026-05-28 16:08:50 +0100

    Update provider configurations for Terraform

--- 01-provider.tf
+++ 01-provider.tf
@@ -1,28 +1,28 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-
 terraform {
   required_providers {
-    namecheap = {
-      source = "namecheap/namecheap"
-      version = "2.2.0"
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.0.0-beta2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
     }
   }
 }
 
-provider "namecheap" {
-  user_name = var.namecheap_username
-  api_user = var.namecheap_api_user
-  api_key = var.namecheap_api_key
-  client_ip = var.namecheap_client_ip
-  use_sandbox = false
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
+
 provider "aws" {
   region = "us-east-1"
 }
